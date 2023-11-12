6afcc9666e173ee56e06fd1a39ea95c57c0593c8
Shivpalsingh15 <singhshivpal.coder@gmail.com>
2023-11-12 14:16:31 +0530

    Basic concepts of JS (My first commit)

---
+++ index.js
@@ -0,0 +1,142 @@
+//data types 
+
+// 2 typeof data types in JS
+// Primitive      Non - primitive
+
+
+// how yo chck the data type in js 
+// 2 ways 
+let str  = '1'
+let numb  =  1
+console.log('data type str:', typeof str) // 1 way 
+console.log('data type  numb:', typeof numb) // 1way 
+console.log('data type str:', typeof(str)) // 2nd  way 
+console.log('data type  numb:', typeof(numb)) // 2nd way 
+
+// truthy & falsy values 
+// 0 = false 
+// 1 = true
+
+//empty array  = true   IMPORTANT !!  
+// arr.length == 0 if arr is empty its length is 0 and 0 is falsy value
+let a  = 0; // 0 is falsy value 
+let b = 1; // 1 is truthy value 
+
+// if(b) and if(b==true) are same style this is called shorthand
+if(b){                              
+    console.log(true)
+}else{
+    console.log(false)
+}
+if(b==true){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+// empty str is falsy value
+let str1 = ''
+console.log('str1:', str1)
+
+if(str1){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+// difference bet true & 'true'
+// value are same but diff is data type true is Boolean and other one is string in js 
+// str with space  =  truly value 
+// if(' '){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+// str without space  {empty string}  =  falsy value 
+
+// if(''){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+// string as 'true' =  truly value 
+
+// if('true'){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+// string as 'false' =  truly value 
+// but false is Boolean = falsy value 
+// below example: 
+if('false'){
+    console.log(true)
+}else{
+    console.log(false)
+}
+if(false){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+// array 
+let arr1 = [] // empty array   // truthy value  
+let arr2 = [1,2]  // nornal aaray 
+
+// if else can be used as urinary operator 
+let ans = arr1 ? true : false   // syntax remeber     condition ? true : false
+                                // if condition is true thenthis will happen else : after colon will happen
+
+
+// console.log(ans)  //  output  true 
+// if(arr1 == true){
+//     console.log(true ,'skuhcsjdhc')
+// }else{
+//     console.log(false , 'cjg6765vg')
+// }
+// output will be true here 
+// console.log([]?true: false)
+// console.log(arr1?true: false)
+// SAME  output will be true here   this is urinary format 
+// BUT    -- length of empty arr is 0 & 0 is falsy value
+console.log([].length ? true : false)  // false
+console.log(arr1.length ? true : false) // false 
+// empty obj is also true ALSO !!
+
+// why empty [] is  truthy 
+// bczo it is non promitive adarta type and they are called as refrenence data type 
+// they store the address they are not counted as value 
+// this is one if the difference between primitive & non primitive data type 
+// empty string is false as js is checking the value but [] is true as js is checking not value but as refrence (adddress )
+
+console.log('1'=== 1) // false as checking data type also  
+console.log('1' == 1) // only checking value so true 
+
+console.log([]==[])  // false
+console.log({}=={})  // false
+//console.log([] === [])  // false This condition will always return 'false' since JavaScript compares objects by reference, not value.
+
+
+// undefined and null
+console.log(null ? 'true hai ': 'true nhi hai ')
+// null is falsy value 
+console.log(undefined ? 'true hai ': 'true nhi hai ')
+// undefined is falsy value 
+// var myVariable = null;
+// var myOtherVariable;
+// if (myVariable === null) {
+//   console.log('true....')
+// } 
+// // true 
+// if (myVariable == undefined) {
+//   console.log('true......')
+// }
+
+// let m  = 'n'
+// let M = 'N'
+// console.log(typeof m === typeof M?true : false)
+
+console.log(!null ? true: false)
---
+++ index2.js
@@ -0,0 +1,94 @@
+//template literal
+let str  = 'shiv is good boy'
+// console.log('str:', str)
+let str1  = `shiv is good boy1`
+// console.log('str1:', str1)
+
+// actually there is no such differ in seeing it
+// but we use tmeplate litera to use any value / variable in between string
+
+let name2  = 'Nilesh'
+let place = 'indore'
+let age  = 24
+
+let str2 = 'My name is' + ' '+ name2 + " " + 'I live in ' + place + ' and may age is ' + age
+// console.log('str2:', str2)
+
+// above method is normal    {  CONCATENATION  }
+// but can be achieve using template literals
+
+let str3 = `My name is ${name2} and may age is ${age} and I live in ${place}`
+// console.log('str3:', str3)
+
+/// checking the type of data type with other 
+// number & string 
+
+/*
+String + String  = String
+String + Number  = String
+Number + Number  = Number
+NUmber + String  = String 
+*/
+
+let num  = 1;
+let num2  = '1';
+let st  = '1';
+let st2  = '2';
+// console.log(typeof(st+num)) // 11
+// console.log(typeof(num+st)) // 11
+// console.log((num+num2)) // 2
+// console.log(st2+st) 
+
+
+// let  a = 12;
+// let  b = "asdf";
+// console.log(a*b)      // NaN not a number 
+
+// console.log(NaN=='a'?true : false)
+
+
+// SWITCH STATEMENT ---- 
+// kind of concept in js or function in js which takes expressions or value and it has cases if the expression  match with case then code will run accordingly to tht case & there is break also to break the code and it will not check to other casee if break is not use then it will check all cases and there is default case we have to use to tell code if no cases match with expression then default will run !
+
+// SYNTAX IS VERY IMPORTANT
+const expr = 'Papayas';
+switch (expr) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+  case 'Mangoes':
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    // Expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
+
+
+/*
+
+
+
+
+
+
+*/
+
+
+
+
+// if(a==1){
+
+// }
+
+
+function myFunc(theArr) {
+  theArr[0] = 30;
+}
+
+const arr = [45];
+
+console.log(arr[0]); // 45
+myFunc(arr);
+console.log(arr[0]); // 30
