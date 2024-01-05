9516854171cdedd3acc1187f06cc049ce9a90909
Shivpalsingh15 <singhshivpal.coder@gmail.com>
2024-01-05 08:20:43 +0530

    practise

---
+++ js/console.js
@@ -0,0 +1,117 @@
+  document.getElementById("h").innerHTML=("javascript");
+  document.write("hello world!");
+  console.log("hello world!");
+  console.log("jay shree mahakal");
+ let a= 5;
+  let b= 5;
+  console.log("sum =", a+b );
+  let pencil= 10;
+  let pen= 20;
+  let output=`total = ${pencil+pen}RS`;
+  console.log(output);
+  console.log("total =", (pencil+pen) , 'rupees');
+  let age= 18;
+  console.log(age<20);
+  let color= "yellow";
+
+  if (color=="red")
+  {
+      console.log("Stop"); }    
+  if(color=="green"){
+      console.log("Go slow");
+  }
+  if(color=="yellow"){
+      console.log("Go" );
+  
+
+  let hello = "Hello";
+  let age=15;
+  if(age>=18)
+  {
+      console.log("vote");
+  }
+  else if(age<=18)
+  {
+      console.log("no vote");
+  }
+
+  let session= "winter";
+
+  if(session=="rainy"){
+      console.log("barish");
+  }
+  else if(session=="monsoon"){
+      console.log("thand");
+  }
+  else if(session=="rainy"){
+      console.log("barish");
+  }
+  else(
+      console.log('kuch nhi')
+
+
+  let size= 'x';
+
+  if (size=='xl')
+  {
+      console.log(250);
+  }
+
+
+  else(console.log( "NOT AVAILABLE")
+  );
+
+
+  let marks= 43;
+
+  if(marks>=33)
+  {
+      console.log("PASSED");
+      if(marks>=33){
+          console.log("grade A");
+
+      }
+  }
+  else (
+      console.log("FAILED")
+  )
+
+
+  let marks= 75;
+
+  if ((marks>=33 && marks<= 80) || !True) 
+  {
+      console.log('passes');
+      console.log('grade a+');
+  }
+
+  let str = "ae";
+
+  if (undefined)
+  {
+      console.log('TRue');
+  }
+  else{
+      console.log('False')
+  }
+    
+  alert("Success");
+  alert("jay h0");
+  console.log("passes");
+  console.error("NS");
+  console.warn("NS");
+  let 
+
+  let msg = ("    Hello    ");
+ 
+  let x = 5;
+  let y = 5;
+  let z = x*y;
+  console.log('z');
+
+ let color =5;
+ if (color >= 3) 
+ {
+     console.log('true');
+ }
+ else {console.log('false')};
---
+++ js/pr.js
@@ -0,0 +1,80 @@
+let a = "Hello world!";
+console.log(a);
+
+// let n = prompt("Please enter number");
+// n = parseInt(n);
+
+
+// for (let i = n; i <= n*10; i=i+n) {
+//     console.log(i);}
+// for (var r = 15; r >= 1; r=r-2) {
+//     console.log(r);}
+
+//  for (let i = 1; i <= 3; i++) {
+    
+//  for (let j = 1; j <= 3; j++){
+//     console.log(j);
+// }
+//  }
+
+// let i= 1;
+
+// while (i <= 10) {
+//     console.log(i);
+//     i=i+2;}
+
+// const favmovie = "om";
+
+// let guess = prompt("Please enter movie name");
+
+// while ((guess != favmovie) && (guess!="Quit")) {
+//     guess = prompt("try again");
+// }
+
+//  if(guess == favmovie) {
+//     console.log("Congratulations!");
+// }
+//     else {
+//         console.log("You Failed!");
+//     }   
+
+// for(i=0; i<=20; i++){
+// if(i<5){
+// console.log(i)
+// }else if(i=5 && i<=10){
+// console.log('nilesjh')
+// }else{
+// consol.log('takuu')
+// }
+// }
+
+
+// i= 1;
+// while (i <=8){
+//     if(i==5){
+//         break
+//     }
+//     console.log(i)
+//     i++
+// }
+
+
+// let fruits = ['mango', 'banana', 'apple', 'orange', 'jam']
+// fruits.push('pie')
+// break
+// for (let i=fruits.length-1; i>=0; i--) {
+//     console.log(i, fruits[i])
+//     break
+// }
+
+    // let i = "document";
+
+    // for (i of "document"){
+    //     console.log(i)
+    // }
+
+
+  let todo = [];
+  
+  let req = prompt('write our tasks ');
+  console.log(req);
